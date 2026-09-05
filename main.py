3aa258185f2eda2e9c5e496ccfa6028c7e159e22
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-05 14:44:16 +0530

    controling player movement

--- main.py
+++ main.py
@@ -6,21 +6,33 @@ WIDTH, HEIGHT = 900, 650
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Dodge')
 BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
+PLAYER_WIDTH = 30
+PLAYER_HEIGHT = 50
+PLAYER_VEL = 5
+clock = pygame.time.Clock()
 
-def draw():
+def draw(player):
     WIN.blit(BG, (0, 0))
+    pygame.draw.rect(WIN, 'red', player)
     pygame.display.update()
 
 def main():
     run = True
+    player  = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
+            player.x += PLAYER_VEL
 
-        draw()
+        draw(player)
 
     pygame.quit()
 
