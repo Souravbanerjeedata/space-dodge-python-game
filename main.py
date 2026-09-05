bcdb672f0ec27ad0e4d099f6610bcfc1630b368c
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-05 15:22:49 +0530

    moving projectiles

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 PLAYER_WIDTH = 30
 PLAYER_HEIGHT = 50
 PLAYER_VEL = 5
+STAR_WIDTH = 10
+STAR_HEIGHT = 20
 FONT = pygame.font.SysFont("comicsans", 30)
 
 def draw(player, elapsed_time):
@@ -25,14 +27,29 @@ def main():
     clock = pygame.time.Clock()
     start_time = time.time()
     elapsed_time = 0
+    star_add_increment = 2000
+    star_count = 0
+
+    stars = []
 
     while run:
-        clock.tick(60)
+        star_count += clock.tick(60)
         elapsed_time = time.time() - start_time
+
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
             player.x -= PLAYER_VEL
