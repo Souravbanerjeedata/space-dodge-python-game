a046d86e03af7410742ba44eb919811643f7ad76
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-05 13:26:00 +0530

    adding the background to the display

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pygame
 import time
 import random
 
-WIDTH, HEIGHT = 900, 600
+WIDTH, HEIGHT = 900, 650
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Dodge')
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
 
 def main():
     run = True
@@ -14,6 +19,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw()
 
     pygame.quit()
 
