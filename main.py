d66d8f1ebaddafb585ecc52d7acd3929af846a3f
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-05 14:56:31 +0530

    rendering elapsed time on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 900, 650
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -9,19 +10,25 @@ BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 PLAYER_WIDTH = 30
 PLAYER_HEIGHT = 50
 PLAYER_VEL = 5
-clock = pygame.time.Clock()
+FONT = pygame.font.SysFont("comicsans", 30)
 
-def draw(player):
+def draw(player, elapsed_time):
     WIN.blit(BG, (0, 0))
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
     pygame.draw.rect(WIN, 'red', player)
     pygame.display.update()
 
 def main():
     run = True
     player  = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
 
     while run:
         clock.tick(60)
+        elapsed_time = time.time() - start_time
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -32,7 +39,7 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
             player.x += PLAYER_VEL
 
-        draw(player)
+        draw(player, elapsed_time)
 
     pygame.quit()
 
